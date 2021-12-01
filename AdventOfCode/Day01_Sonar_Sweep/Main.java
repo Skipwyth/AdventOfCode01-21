91129f212c6a695d793eb6344a5941b5ead42621
Skipwyth <71076324+Skipwyth@users.noreply.github.com>
2021-12-01 19:47:40 +0100

    Add files via upload

---
+++ AdventOfCode/Day01_Sonar_Sweep/Main.java
@@ -0,0 +1,14 @@
+
+//--- Day 1: Sonar Sweep ---
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		NumberArray test = new NumberArray();
+
+		System.out.println(test.smaller());
+
+	}
+
+}
